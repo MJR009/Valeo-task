da4d54d50fe334237d312cc766e947ebcbdc2c4b
MJR009 <marty.jab@seznam.cz>
2025-03-28 18:36:29 +0100

    feat: added first half of f_cam entry generators

--- f_cam.py
+++ f_cam.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import csv
+import random
 
 def printUsage():
     print("\033[36mUsage: python3 f_cam.py <output directory>\033[39m")
@@ -23,15 +25,50 @@ def main():
 def writeCSV(path):
     file = open(path, "w")
 
-    file.write("test")
+    csvHandle = csv.writer(file)
+    csvHandle.writerow(
+        ["Timestamp", "FrameID", "Speed", "YawRate", "Signal1", "Signal2"]
+    )
+
+    timestamp = genTimestamp()
+    frameID = genFrameID()
+    speed = genSpeed()
+
+    for _ in range(2_000):
+        csvHandle.writerow(
+            [
+                next(timestamp),
+                next(frameID),
+                next(speed),
+                "",
+                "",
+                ""
+            ]
+        )
 
     file.close()
 
-# cols - Timestamp, FrameID, Speed, YawRate, Signal1, Signal2
+def genFrameID():
+    id = 100
+    while True:
+        yield id
+        id += 1
 
-# timestamp - float [us], first 100s -> 100 000 000us, inc. 27.7ms -> 27 700us, +-0.05ms -> +- 50us, 6 decimal places
-# frameid - icremental id, start 100, end 2100
-# speed - float [kmph], start 60, increment 0.08 kmph up to 120, then always 120 +- 0.05 kmph
+def genTimestamp():
+    timestamp = 100_000_000.0
+    increment = 27_700.0
+    while True:
+        yield round(timestamp, 6)
+        timestamp += increment + random.uniform(-50.0, 50.0)
+
+def genSpeed():
+    speed = 60.0
+    increment = 0.08
+    while True:
+        yield speed
+        speed = speed + increment if ( speed < 120.0 ) else 120.0 + random.uniform(-0.05, 0.05)
+        # < used, <= (or ==) on float is invalid
+
 # yawrate - float [°/s], start 0, then in range od +- 1 every frame # per each frame? not english
 # signal1 - int, 0 up to id 200, for > 200 select random from 1 - 15 and keep constant
 # signal2 - signal1 < 5 -> 0; signal1 >= 5 80 +- 10 every frame
